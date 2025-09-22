3f9a6c2e006efdea7f842ed4e8f8c6b047edb728
小米里的大麦 <hc1960074081@qq.com>
2025-09-22 22:01:34 +0800

    2

---
+++ day 21/2. 牛客：JD7 集合.cpp
@@ -0,0 +1,33 @@
+﻿// 牛客：JD7 集合
+// 链接：https://www.nowcoder.com/practice/635ff765d4af45b5bf8e3756ed415792?tpId=134&tqId=33860&ru=/exam/oj
+
+// 方法：去重排序
+#include <iostream>
+#include <set>
+using namespace std;
+
+int n, m, x;
+set<int> s;
+
+int main()
+{
+    cin >> n >> m;
+    while (n--)
+    {
+        cin >> x;
+        s.insert(x);
+    }
+
+    while (m--)
+    {
+        cin >> x;
+        s.insert(x);
+    }
+
+    for (auto x : s)
+    {
+        cout << x << ' ';
+    }
+
+    return 0;
+}
